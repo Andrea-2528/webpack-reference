d0fe053d11595d2d9a33a61edfd48dc8acd4b0f2
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2025-01-22 15:10:03 +0100

    Update template

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import js from "@eslint/js";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  js.configs.recommended,
-
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
